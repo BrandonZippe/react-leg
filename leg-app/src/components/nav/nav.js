051f26b176e276f27f6863aa8b24653663caf1f8
BrandonZippe <BrandonZippe@users.noreply.github.com>
2020-09-23 18:23:38 -0500

    nav click event handler

--- leg-app/src/components/nav/nav.js
+++ leg-app/src/components/nav/nav.js
@@ -11,25 +11,40 @@ class Nav extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: false,
+      allInActive: false,
+      currentActive: false,
     };
-    this.toggleClass = this.toggleClass.bind(this);
+    this.handleAllInClick = this.handleAllInClick.bind(this);
+    this.handleCurrentClick = this.handleCurrentClick.bind(this);
   }
 
-  toggleClass() {
-    const currentState = this.state.active;
-    console.log(this.state.active);
-    this.setState({
-      active: !currentState
-    });
+  handleAllInClick() {
+    if (!this.state.allInActive) {
+      this.setState((prevState) => ({
+        ...prevState,
+        allInActive: !prevState.allInActive,
+        currentActive: !prevState.allInActive,
+      }));
+    }
+  };
+
+  handleCurrentClick() {
+    if (this.state.currentActive) {
+      this.setState((prevState) => ({
+        ...prevState,
+        currentActive: !prevState.currentActive,
+        allInActive: !prevState.currentActive,
+      }));
+    }
   };
 
 
   render () {
+    const { allInActive, currentActive } = this.state;
     return (
       <nav>
-        <NavItem id="allTime" isOn={this.state.active ? 'on' : null} text="All Time Members" onClick={() => this.toggleClass()} />
-        <NavItem id="current" isOn="on" text="Current Members" onClick={() => this.toggleClass()} />
+        <NavItem id="allTime" isOn={allInActive ? 'on' : null} text="All Time Members" onClick={this.handleAllInClick} />
+        <NavItem id="current" isOn={!currentActive ? 'on' : null} text="Current Members" onClick={this.handleCurrentClick} />
       </nav>
     );
   }
