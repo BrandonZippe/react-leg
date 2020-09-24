7759159f6d485660e6bd8f185fa92340ba62d2aa
BrandonZippe <BrandonZippe@users.noreply.github.com>
2020-09-24 10:48:47 -0500

    refactor sorts a list component

--- leg-app/src/components/sorts/sorts.js
+++ leg-app/src/components/sorts/sorts.js
@@ -1,25 +1,87 @@
 import React from 'react';
 import './sorts.scss';
 
+const sortInactiveClass = 'sortBtn';
+const sortActiveClass = sortInactiveClass + ' selected';
+const sorts = [
+  {
+    id: 'data-wp',
+    css: sortActiveClass,
+    content: 'Win %'
+  },
+  {
+    id: 'data-win',
+    css: sortInactiveClass,
+    content: 'Wins'
+  },
+  {
+    id: 'data-title',
+    css: sortInactiveClass,
+    content: 'Titles'
+  },
+  {
+    id: 'data-pf',
+    css: sortInactiveClass,
+    content: 'Points For'
+  },
+  {
+    id: 'data-pa',
+    css: sortInactiveClass,
+    content: 'Points Against'
+  },
+  {
+    id: 'data-tb',
+    css: sortInactiveClass,
+    content: 'Toilet Bowls'
+  }
+];
+
 function SortItem(props) {
   return (
-    <span id={props.id} className={props.cssClass} onClick={props.onClick}>{props.text}</span>
+    <span id={props.id} className={props.className} onClick={props.onClick}>{props.text}</span>
+  );
+}
+
+function SortsNav(props) {
+  const sorts = props.sorts;
+  const sortItems = sorts.map((sort) =>
+    <SortItem
+      key={sort.id}
+      id={sort.id}
+      className={sort.css}
+      onClick={props.onClick}
+      text={sort.content}
+    />
+  );
+
+  return (
+    <nav>
+      {sortItems}
+    </nav>
   );
 }
 
 
-function Sorts() {
-  const cssName = 'sortBtn';
-  return (
-    <nav>
-      <SortItem id="data-wp" cssClass={cssName + " selected"} text="Win %" />
-      <SortItem id="data-win" cssClass={cssName} text="Wins" />
-      <SortItem id="data-title" cssClass={cssName} text="Titles" />
-      <SortItem id="data-pf" cssClass={cssName} text="Points For" />
-      <SortItem id="data-pa" cssClass={cssName} text="Points Against" />
-      <SortItem id="data-tb" cssClass={cssName} text="Toilet Bowls" />
-    </nav>
-  );
+class Sorts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+  }
+
+
+  render () {
+    return (
+      <SortsNav sorts={sorts} onClick={this.handleClick}/>
+    );
+  };
+
 }
 
 export default Sorts;
